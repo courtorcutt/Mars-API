ccac3b2f965e9d8bcb91915439349783ced6b34b
Courtney Orcutt <18cro@queensu.ca>
2022-01-24 16:19:15 +0000

    functionality update

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Menu, Header, Button } from "semantic-ui-react";
 
-export default function NavBar() {
+// DatePicker requires moment.js
+import moment from "moment";
+
+// the NavBar will import the date button and datepicker components
+import DateInput from "./DateInput.js";
+
+export default class Navbar extends Component {
+  render() {
     return (
-        <div className="navbar">
-            <ul>
-                <Link className="link" to="/" exact >Click Here to Look Through the Telescope Again! Or Choose a Different Date: </Link>
-                    <input type="date" id="date-picker" name="trip-start" value="2022-01-19" min="2018-01-01" max="2022-01-31"></input>
-            </ul>
+      <React.Fragment>
+        <div className = "navbar">
+              <Header as="h1" id="start-title" onClick={() => 
+                this.props.handleChange(moment())}>
+                HOME
+              </Header>
+              <DateInput
+                photo={this.props.photo}
+                setPhoto={this.props.setPhoto}
+                date={this.props.date}
+                handleClick={this.props.handleClick}
+                handleChange={this.props.handleChange}
+              />
+              {this.props.viewingFavorites ? (
+                <Button>Sort Favorites by Date</Button>
+              ) : (
+                <Button id="favourite-button" onClick={this.props.viewFavorites}>View Favorites</Button>
+              )}
         </div>
-    )
+      </React.Fragment>
+    );
+  }
 }
